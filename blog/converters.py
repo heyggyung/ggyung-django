564685f88a2e5060bcb6e92bcf0081c3ebcec0f5
heyggyung <heyggyung@gmail.com>
2019-02-13 11:37:50 +0900

    update slugconverter

--- blog/converters.py
+++ blog/converters.py
@@ -1,4 +1,8 @@
+from django.urls.converters import StringConverter
 
+class SlugUnicodeConverter(StringConverter):
+    regex = r'[-/\w]+'
+        
 class FourDigitYearConverter:
     regex = r'2[01]\d{2}'
 

842c96601cd406d2e2190c3d615b3ebacf3fcbbe
heyggyung <heyggyung@gmail.com>
2019-02-13 12:26:27 +0900

    add shop_app

---
+++ blog/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from blog.views import index, hello_times
+from blog.views import articles_by_year
+
+from django.urls import register_converter
+from blog.converters import FourDigitYearConverter
+from blog.converters import SlugUnicodeConverter
+
+register_converter(FourDigitYearConverter, 'year')
+register_converter(SlugUnicodeConverter, 'slug_unicode')
+
+app_name = 'blog' # URL reverse 기능에서 사용
+
+urlpatterns = [
+    path('articles/<year:year>/', articles_by_year),
+
+    # re_path('^blog/1/$', post_detail),
+    # re_path('^blog/1/edit/$', post_edit),
+ 
+    path('hello_times/<int:times>/', hello_times),
+    path('', index),
+]
